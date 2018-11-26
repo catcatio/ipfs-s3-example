8ca86192c37a300b9c743308b6ec63538f73d934
NiponChi <nipon.chi@gmail.com>
2018-11-27 00:42:32 +0700

    Added deploy script and code clean up

--- src/routers/index.js
+++ src/routers/index.js
@@ -1,9 +1,6 @@
 import { Router } from 'express'
 import AESCrypto from '../AESCrypto'
 import { Duplex } from 'stream'
-// import { bufferToStream } from '../buffer'
-
-// const { bufferToStream } =  require('../buffer')
 
 const bufferToStream = (buffer) => {
   const stream = new Duplex()
@@ -29,7 +26,7 @@ const upload = (ipfs) => async (req, res) => {
   const files = req.files
   const result = await doUpload(ipfs, files.upload.name, files.upload.data)
   console.log(result)
-  res.json(result.hash)
+  res.json(result)
 }
 
 const supload = (ipfs, defailtFileKey) => async (req, res) => {
@@ -37,7 +34,7 @@ const supload = (ipfs, defailtFileKey) => async (req, res) => {
   const fileKey = req.query.fileKey
   const result = await doUpload(ipfs, files.upload.name, files.upload.data, fileKey || defailtFileKey)
   console.log(result)
-  res.json(result.hash)
+  res.json(result)
 }
 
 const ipfsDownloadStream = (ipfs, ipfspath) => {
@@ -56,6 +53,7 @@ const download = (ipfs) => async (req, res) => {
 }
 
 const sdownload = (ipfs, defailtFileKey) => async (req, res) => {
+  const startTime = Date.now()
   const ipfspath = req.path
   const fileKey = req.query.fileKey
   const aesCrypto = AESCrypto(fileKey || defailtFileKey)
@@ -68,6 +66,9 @@ const sdownload = (ipfs, defailtFileKey) => async (req, res) => {
       res.status(400).send(err)
     })
       .pipe(aesCrypto.decryptStream())
+      .on('end', () => {
+        console.log('s-download done', Date.now()-startTime)
+      })
       .pipe(res)
   } catch (error) {
     console.log(error)
